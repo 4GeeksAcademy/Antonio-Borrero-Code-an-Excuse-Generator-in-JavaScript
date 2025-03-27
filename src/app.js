1224ab71f496c63c8a2a667a0a4e185f3c042ae1
Antonio Borrero <antojoseabad@gmail.com>
2025-03-27 18:06:07 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["my dog ", "my grandpa ", "my neighbour ", "superman "];
+  let action = ["ate ", "peed ", "broke ", "burned "];
+  let what = ["my homework ", "my car ", "my house ", "the street "];
+  let when = ["this morning ", "last night ", "yesterday ", "during my lunch break "]
+  
+  let whorandom = Math.floor(Math.random()*who.length);
+  let actionrandom = Math.floor(Math.random()*action.length);
+  let whatrandom = Math.floor(Math.random()*what.length);
+  let whenrandom = Math.floor(Math.random()*when.length);
+
+  document.querySelector("#excuse").innerHTML = 
+  (who[whorandom] + action[actionrandom] + what[whatrandom] + when[whenrandom]);
 };
